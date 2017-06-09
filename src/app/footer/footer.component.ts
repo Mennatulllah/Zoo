466df1e81ca9bae18c3c35ab37329d343aaf8d97
Mennatulllah <mennatalla.mohammed@gmail.com>
2017-06-09 15:12:21 +0200

    Add files via upload

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,16 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ContactusService } from '../contactus.service';
+import { Contact } from '../../Interfaces/Contacts'
+
 
 @Component({
-  moduleId:module.id,
+  moduleId: module.id,
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css','../../assets/css/contact.css','../../assets/css/footer.css','../../assets/css/animate.css']
 })
 export class FooterComponent implements OnInit {
+  Contacts: any[];
 
-  constructor() { }
+  contacts : Contact[]=[];
+  constructor(private contactServ: ContactusService) { }
 
   ngOnInit() {
+     this.contactServ.getAllContacts().subscribe(Contacts => { 
+      this.contacts = ( <Contact[]> Contacts).filter(a=>a.zoo == '59131ee02ac34812fc941e7f'); 
+       console.log(this.contacts[0].phone);
+   })
   }
 
 }
+
+
