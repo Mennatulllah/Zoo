a6d192239c94ed293740a24ea352af36480ca9eb
MonaMatar <monamatar21@hotmail.com>
2017-06-09 16:51:10 +0200

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { ZooRoutModule } from 'appRoutingModule.module';
 
+/// components 
 import { AppComponent } from './app.component';
 import { FeedbackComponent } from './feedback/feedback.component';
 import { FaqComponent } from './faq/faq.component';
-import { ZooRoutModule } from 'appRoutingModule.module';
 import { DidUknowComponent } from './did-uknow/did-uknow.component';
-
-import { FaqService } from './faq.service';
-import { ZoosService } from './zoos.service';
-import { NewsService } from './news.service';
-import { AnimalsService } from './animals.service';
-import {TicketsService} from './tickets.service'
-
 import { ZoosComponent } from './zoos/zoos.component';
 import { PreservesComponent } from './preserves/preserves.component';
 import { TicketsComponent } from './tickets/tickets.component';
@@ -27,11 +21,21 @@ import { TipsComponent } from './tipsforyouvisit/tips.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
-import { GalleryComponent } from './gallery/gallery.component';
-import { AnimalSearchPipe } from './animal-search.pipe'
+import { GalleryComponent } from './gallery/gallery.component'
+
+// services
+
+import { FaqService } from './faq.service';
+import { ZoosService } from './zoos.service';
+import { NewsService } from './news.service';
+import { AnimalsService } from './animals.service';
+import {TicketsService} from './tickets.service';
+import {FaqTypeService} from './FAQType.service';
+import {PreserveService} from './preserve.service';
 
 
-
+// pipes
+import {FAQFilterPipe} from '../pipes/FAQFilter.pipe'
 
 @NgModule({
   declarations: [
@@ -50,7 +54,7 @@ import { AnimalSearchPipe } from './animal-search.pipe'
     FooterComponent,
     HomeComponent,
     GalleryComponent,
-    AnimalSearchPipe
+    FAQFilterPipe
     
   ],
   imports: [
@@ -59,7 +63,15 @@ import { AnimalSearchPipe } from './animal-search.pipe'
     HttpModule ,
     ZooRoutModule
   ],
-  providers: [ NewsService, FaqService , ZoosService, AnimalsService,TicketsService],
+  providers: [
+    NewsService,
+    FaqService ,
+    ZoosService,
+    AnimalsService,
+    TicketsService,
+    FaqTypeService,
+    PreserveService
+    ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
